c75804371ff60bb7536a0fe908706e8f47925bc7
Tali98 <tali_lagos@hotmail.com>
2018-05-25 13:33:24 -0600

    Cree clases Usuario y clases

---
+++ Examen1/src/examen1/Usuario.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examen1;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author tali_
+ */
+public class Usuario {
+    private String nombre_completo;
+    private int edad;
+    private String correo;
+    private String user;
+    private String contrasena;
+    ArrayList<clases>lista = new ArrayList();
+}
---
+++ Examen1/src/examen1/clases.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examen1;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author tali_
+ */
+public class clases {
+    private String clase_name;
+    ArrayList<String>atributos = new ArrayList();
+    ArrayList<String>metodos = new ArrayList();
+     ArrayList<String>type  = new ArrayList();
+    
+}
